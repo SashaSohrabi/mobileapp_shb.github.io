9183b14f06900863f232246b4f3f81d26eedd99e
sashasohrabi <shb.sohrabi@gmail.com>
2019-01-06 15:49:06 +0200

    chamges in main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,61 @@
-$(document).ready (function() {
+(function() {
+  $(document).ready(function() {
     new WOW().init();
-    $(".header-menu__burger-nav").on("click", function(){
-
-        $(".header-menu__nav").toggleClass("open");
-
+    $(".header-menu__burger-nav").on("click", function() {
+      $(".header-menu__nav").toggleClass("open");
     });
 
-    $('.header-content__imgs').waypoint(function() {
+    $(".header-content__imgs").waypoint(
+      function() {
+        $(".header-content__imgs").addClass("animated slideInUp");
+      },
+      {
+        offset: "70%"
+      }
+    );
 
-        $('.header-content__imgs').addClass('animated slideInUp');
+    $(".iphone__on").waypoint(
+      function() {
+        $(".iphone__on").addClass("animated fadeIn");
+      },
+      {
+        offset: "70%"
+      }
+    );
 
-    }, {
-        offset: '70%'
-    });
+    $(".iphone__btn")
+      .delay(2300)
+      .animate({ bottom: "+=-3" }, 300);
+    $(".iphone__btn")
+      .delay(300)
+      .animate({ top: "+=-3" }, 100);
+  });
+})();
 
-    $('.iphone__on').waypoint(function() {
+//====================ScrollMagic=====================
+(function() {
+  var controller = new ScrollMagic.Controller();
 
-        $('.iphone__on').addClass('animated fadeIn');
-
-    }, {
-        offset: '70%'
-    });
-
-    $('.iphone__btn').delay(2300).animate( {bottom: "+=-3"}, 300);
-    $('.iphone__btn').delay(300).animate( {top: "+=-3"}, 100);
-
-});
-
-//=========================================
-
-var controller = new ScrollMagic.Controller();
-
-var scene7 = new ScrollMagic.Scene({
-    triggerElement: '#hero'
-}).setClassToggle('.download', 'animation')
+  var scene7 = new ScrollMagic.Scene({
+    triggerElement: "#hero"
+  })
+    .setClassToggle(".download", "animation")
     .addTo(controller);
 
-//==========================================
+  $(".fade-in").each(function() {
+    // build  a tween
+    var tween = TweenMax.from($(this), 0.3, {
+      autoAlpha: 0,
+      scale: 0.5,
+      y: "+=30",
+      ease: Linear.easeNone
+    });
 
-$('.fade-in').each(function () {
-
-// build  a tween
-    var tween = TweenMax.from($(this), 0.3, {autoAlpha: 0, scale: 0.5, y: '+=30', ease: Linear.easeNone});
-
-// build a scene
+    // build a scene
     var scene3 = new ScrollMagic.Scene({
-        triggerElement: this
+      triggerElement: this
     })
-        .setTween(tween)
-        .addTo(controller);
-});
+      .setTween(tween)
+      .addTo(controller);
+  });
+})();
